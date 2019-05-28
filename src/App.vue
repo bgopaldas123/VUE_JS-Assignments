0fe64d27d5434813dbdef5252718b016306362e9
gopal.das@fissionlabs.com <bgopaldas123>
2019-05-28 17:05:29 +0530

    first assignment in vue js

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="app">
+    <Demo1 :node="root"/>
+  </div>
+</template>
+
+<script>
+import Demo1 from './components/Demo1'
+
+export default {
+  components: {
+    Demo1
+  },
+  name: 'app',
+  data () {
+    return {
+      root: 
+        {
+        name: 'Click Me',
+        children: [
+          {
+            name: 'Mobile',
+            children: [
+              {
+                name: 'Apple',
+                children: [
+                  {
+                    name: 'XS'
+                  },
+                  {
+                    name: 'XS Max'
+                  }
+                ]
+              },
+              {
+                name: 'Blackberry',
+                children: [
+                  {
+                    name: 'Evolve'
+                  },
+                  {
+                    name: 'Elite'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'TV',
+            children: [
+              {
+                name: 'Samsung',
+                children: [
+                  {
+                    name: 'Samsung1'
+                  },
+                  {
+                    name: 'Samsung2'
+                  }
+                ]
+              },
+              {
+                name: 'Sony',
+                children: [
+                  {
+                    name: 'Sony1'
+                  },
+                  {
+                    name: 'Sony2'
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            name: 'Cars',
+            children: [
+              {
+                name: 'Volkswagen',
+                children: [
+                  {
+                    name: 'Passat'
+                  },
+                  {
+                    name: 'Polo'
+                  }
+                ]
+              },
+              {
+                name: 'Hyundai',
+                children: [
+                  {
+                    name: 'Creta'
+                  },
+                  {
+                    name: 'Xcent'
+                  }
+                ]
+              }
+            ]
+          },
+          
+        ]
+      }
+      
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/Demo1.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div 
+            @click="expandIcon"
+            :style="{'margin-left': `${depth * 20}px`}"
+        >
+        <h1>{{node.name}}</h1>
+        </div>
+        <Demo1
+            v-if="expanded"
+            v-for="(child, index) in node.children" :key="index"
+            :node="child"
+            :depth = "depth + 1"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Demo1',
+    props: {
+        node: Object,
+        depth: {
+            type: Number,
+            default: 0
+        }
+    },
+    data() {
+        return {
+            expanded: false,
+        }
+    },
+    methods: {
+      expandIcon(){
+        this.expanded = !this.expanded;
+      }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
